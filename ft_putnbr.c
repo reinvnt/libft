4cb28ca1bed6470792a4c1a3beee61b1fe19cf32
Reinhardt Venter <rventer@e1z2r7p9.42.us.org>
2019-10-30 21:52:56 -0700

    You left out the semi colon at the end and you had a typo in void

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,26 +6,26 @@
 /*   By: rventer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 14:15:59 by pbie              #+#    #+#             */
-/*   Updated: 2019/10/30 21:44:13 by rventer          ###   ########.fr       */
+/*   Updated: 2019/10/30 21:51:29 by rventer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-vod		ft_putnbr(int n)
+void		ft_putnbr(int n)
 {
-	if (n == -2147483648)
-		ft_putstr("-2147483648")
+	if (n == -2147483648);
+		ft_putstr("-2147483648");
 	else if (n < 0)
 	{
-		ft_putchar('-')
-		ft_putnbr(-n)
+		ft_putchar('-');
+		ft_putnbr(-n);
 	}
 	else if (n >= 10)
 	{
 		ft_putnbr(n / 10);
-		ft_putchar(n % 10 + '0')
+		ft_putchar(n % 10 + '0');
 	}
 	else
-		ft_putchar(n + '0')
+		ft_putchar(n + '0');
 }
